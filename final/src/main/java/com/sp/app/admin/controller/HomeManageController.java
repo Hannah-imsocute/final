ef14348142831568b335786f4dd66fa5d74eec1f
hannah-imsocute <hannah011221@gmail.com>
2025-02-05 15:59:46 +0900

    commit 한나

--- final/src/main/java/com/sp/app/admin/controller/HomeManageController.java
+++ final/src/main/java/com/sp/app/admin/controller/HomeManageController.java
@@ -2,6 +2,9 @@ package com.sp.app.admin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sp.app.service.AdminReportService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -9,10 +12,19 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/admin/*")
 public class HomeManageController {
 	 
-	@GetMapping("/admin")
+	private final AdminReportService service;
+	
+	@GetMapping("/home")
 	public String handleHome() {
 		return "admin/template";
 	}
+
+	@GetMapping("/report")
+	public String showList() {
+		return "admin/report";
+	}
+	
 }
