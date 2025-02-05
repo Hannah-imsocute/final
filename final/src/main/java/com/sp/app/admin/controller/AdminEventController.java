7be521a658797bfc72dc01fac32ca15eb96377b3
hannah-imsocute <hannah011221@gmail.com>
2025-02-05 16:17:34 +0900

    commit xml

---
+++ final/src/main/java/com/sp/app/admin/controller/AdminEventController.java
@@ -0,0 +1,15 @@
+package com.sp.app.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@RequestMapping("/admin/event")
+@Slf4j
+public class AdminEventController {
+	
+	
+	
+}
