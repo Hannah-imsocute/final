44ee74a2a692632715c25c9c6b2cb4b6fe2ef044
hannah-imsocute <hannah011221@gmail.com>
2025-02-18 17:47:37 +0900

    commit hannah 0218 공지사항 클라이언트 부분 디자인 내일 할거 문의사항 하고 이벤트로 돌아가기 mailsender ignore 했는데 확인 필요

---
+++ final/src/main/java/com/sp/app/service/NoticeService.java
@@ -0,0 +1,20 @@
+package com.sp.app.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.sp.app.admin.model.Notice;
+
+public interface NoticeService {
+	// 필요한 기능 
+		// faq 리스트 불러오기(검색있음), 문의사항 리스트불러오기, 공지사항 리스트 불러오기
+		// data개수 각각 몇개인지 불러오기  
+		
+		public List<Notice> getListOfFaq(Map<String, Object> map);
+		public int DataCountFaq(Map<String, Object>map);
+		
+		public List<Notice> getListOfNotice(Map<String, Object> map);
+		public int DataCountNotice(Map<String, Object> map);
+		
+		public List<String> getCategory();
+}
