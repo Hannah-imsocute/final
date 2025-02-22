085f37f05f6e0f8c30375760e5f77c11024a3594
eomgieun <djarldms12@gmail.com>
2025-02-23 03:20:26 +0900

    누락 파일 commit2

--- final/src/main/java/com/sp/app/mapper/PointMapper.java
+++ final/src/main/java/com/sp/app/mapper/PointMapper.java
@@ -3,8 +3,22 @@ package com.sp.app.mapper;
 import com.sp.app.model.MemberPoint;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Map;
+
 @Mapper
 public interface PointMapper {
 
-    MemberPoint getLatestUserPoint();
+    void insertPointHistory() throws Exception; // 포인트 사용 이력 등록
+    void insertPointHistory(MemberPoint point) throws Exception; // 파라미터 추가
+    void updatePointValid1(Map<String, Object> map) throws Exception; // 사용 포인트만큼 차감
+    void updatePointValid2(Map<String, Object> map) throws Exception; // 포인트 enable 설정
+
+    int getPointEnabled(long memberIdx); // 사용가능한 포인트 금액
+
+    int getUsedPoint(); // 사용금액 반환
+    MemberPoint getUserSaveAmount(long memberIdx); // 포인트 누적 기록 가지고오기
+
+    long getPointSaveNum(long memberIdx);
+
+    long getPointSaveNUm();
 }
