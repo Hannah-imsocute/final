3f90dd4fdf193609be255948fdcd98b747051a0c
eomgieun <djarldms12@gmail.com>
2025-02-27 23:26:53 +0900

    매퍼 누락 commit

---
+++ final/src/main/java/com/sp/app/mapper/ChangeMapper.java
@@ -0,0 +1,16 @@
+package com.sp.app.mapper;
+
+import com.sp.app.model.Change;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface ChangeMapper {
+    void insertChangeRequest(Change change);
+    List<Change> getChangeState(long memberIdx);
+    List<Change> getRequestList(Map<String, Object> map);
+    int dataCount(long memberIdx);
+
+}
